dd9573a402946ded0e8b10da273fafd11fbe7844
psxdev <psxdev@gmail.com>
2016-06-27 16:47:54 +0200

    added echo info

--- compile_toolchain.sh
+++ compile_toolchain.sh
@@ -1,9 +1,14 @@
+echo downloading from dropbox
 wget --no-check-cert https://www.dropbox.com/s/1tbjxc8i9wwbmtx/crossllvm.tar.gz
+echo decompress llvm sources
 gunzip crossllvm.tar.gz
 tar xf crossllvm.tar
 cd crossllvm
 mkdir build
 cd build
+echo preparing cmake
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PS4DEV/toolchain -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-scei-ps4 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCOMPILER_RT_BUILD_BUILTINS:BOOL=OFF -DCOMPILER_RT_BUILD_SANITIZERS:BOOL=OFF -DCOMPILER_RT_CAN_EXECUTE_TESTS:BOOL=OFF -DCOMPILER_RT_INCLUDE_TESTS:BOOL=OFF -DLLVM_TOOL_COMPILER_RT_BUILD:BOOL=OFF  -DCLANG_BUILD_EXAMPLES:BOOL=ON -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_C_FLAGS="-Wdocumentation -Wno-documentation-deprecated-sync" -DCMAKE_CXX_FLAGS="-std=c++11 -Wdocumentation -Wno-documentation-deprecated-sync" -DLLVM_LIT_ARGS="-v" ../llvm
+echo compiling
 cmake --build . -- -j12
+echo installing
 cmake --build . --target install
